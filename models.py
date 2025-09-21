ef4ace63d485e9c22da04863d372bc2a8312c2ff
Crabmann2025 <1990.c@gmx.de>
2025-09-21 17:53:05 +0200

    Database models created

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    movies = db.relationship('Movie', backref='user', lazy=True)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
